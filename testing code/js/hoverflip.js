36b4116c0c70842c157f30efc329153375d29c58
ooddles371 <christo.landry@gmail.com>
2014-12-27 14:46:17 -0500

    upper date

--- testing code/js/hoverflip.js
+++ testing code/js/hoverflip.js
@@ -3,3 +3,22 @@ $(".flip").hover(function(){
   $(this).find(".card").toggleClass("flipped");
   return false;
 });
+
+$(window).resize(function() {
+ if ($(this).width() < 768) {
+   document.getElementById("textbelowrow").innerHTML = "Death by snoo snoo!";
+   
+   
+   
+   //find element
+       //remove element from current parent
+       //make element child of the other parent
+ }
+ else {
+       document.getElementById("textbelowpic").innerHTML = "Death by snoo snoo!";
+	   //do inverse
+ }
+});
+
+//min 992 is medium
+//min 768 is small
