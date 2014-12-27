e53150cde98294bff8f93eebdc95b0396164dd64
ooddles371 <christo.landry@gmail.com>
2014-12-27 16:14:35 -0500

    update

--- testing code/js/hoverflip.js
+++ testing code/js/hoverflip.js
@@ -1,24 +1,63 @@
-/* card flip */
+//--------------------Start Card Flip---------------
 $(".flip").hover(function(){
   $(this).find(".card").toggleClass("flipped");
   return false;
 });
+//--------------------End Card Flip---------------
 
+var small,medium, large;
+small = 0;
+medium = 0;
+large = 0;
+
+if($(window).width() < 768){
+	$('.small').prepend("screen is SMALL");   
+	$('.large').empty();
+	$('.medium').empty();
+	small=2;
+}	else if($(window).width() < 992){
+		$('.medium').prepend("screen is MEDIUM");   
+		$('.large').empty();
+		$('.small').empty();
+		medium=2;
+	}	else{
+			$('.large').prepend("screen is LARGE")
+			$('.small').empty();
+			$('.medium').empty();
+			large=2;
+		}
 $(window).resize(function() {
- if ($(this).width() < 768) {
-   document.getElementById("textbelowrow").innerHTML = "Death by snoo snoo!";
-   
-   
-   
-   //find element
-       //remove element from current parent
-       //make element child of the other parent
- }
- else {
-       document.getElementById("textbelowpic").innerHTML = "Death by snoo snoo!";
-	   //do inverse
- }
-});
+	if ($(this).width() < 768) {
+		small+=1;
+		medium=0;
+		large=0;
+		if (small < 2) {
+			$('.small').prepend("screen is SMALL");   
+			$('.large').empty();
+			$('.medium').empty();
+		}
+	}	else if ($(window).width() < 992){
+			medium+=1;
+			small=0;
+			large=0;
+			if(large < 2) {
+				$('.medium').prepend("screen is MEDIUM")
+				$('.small').empty();
+				$('.large').empty();
+			}
+		}	else {
+				large+=1;
+				small=0;
+				medium=0;
+				if(large < 2) {
+					$('.large').prepend("screen is LARGE")
+					$('.small').empty();
+					$('.medium).empty();
+				}
+			}
+};
 
+
+//min 1200 is large
 //min 992 is medium
 //min 768 is small
