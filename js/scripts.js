ac5ffd728fa99986a2961d299df459ab4ed113e1
ooddles371 <christo.landry@gmail.com>
2015-03-15 19:14:26 -0400

    a

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$(".nav a").smoothScroll({
-		offset: -$("nav").outerHeight()
-	});
-});
-});
--- js/slidedown.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('div.demo-show:eq(1)> div:gt(0)').hide();  
-  $('div.demo-show:eq(1)> h3').click(function() {
-    $(this).next('div:hidden').slideDown('fast')
-    .siblings('div:visible').slideUp('fast');
-  });
-});
